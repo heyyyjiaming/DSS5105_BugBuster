adb0927d167deba7a4fbbb5f9a9646af95bf0c82
Ryanniu02 <e1352057@u.nus.edu>
2024-11-07 20:11:32 +0800

    add

--- streamlit_app_demo.py
+++ streamlit_app_demo.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from openai import OpenAI
 
 st.title("ESG Analysis Tool 📊")
 st.write(
@@ -7,16 +6,11 @@ st.write(
     "To use this app, you may need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.text_input("OpenAI API Key", type="password")
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
 
     # Let the user upload a file via `st.file_uploader`.
     uploaded_file = st.file_uploader(
