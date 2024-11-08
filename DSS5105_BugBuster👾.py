8a28f1651059face4d16a4eaf45e1c5a605fcb3b
Jiaming Ding <e1351662@u.nus.edu>
2024-11-08 17:29:36 +0800

    add main file in streamlit

---
+++ DSS5105_BugBuster👾.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Welcome to DSS5105 Bug Buster's World! 👾")
+st.header("Introduction to the innovative ESG Analysis Tool 📊")
+
+github_url = "https://github.com/heyyyjiaming/DSS5105_BugBuster"
+st.markdown("You can find our GitHub repository [here](%s)"%github_url)
+
+st.subheader("Our Team Members (in alphabetic order):")
+st.markdown("Bi Ying, Chen Zhujun, Ding Jiamig, Huang Yuxin, Li Jingming, Niu Muyuan, Wang Hao, Zhang Yi")
