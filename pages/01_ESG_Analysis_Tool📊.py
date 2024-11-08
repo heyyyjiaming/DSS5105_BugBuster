1c9774ac6b18340118ec36df6a739205d9038826
Jiaming Ding <e1351662@u.nus.edu>
2024-11-08 17:56:12 +0800

    modify page01

--- pages/01_ESG_Analysis_Tool📊.py
+++ pages/01_ESG_Analysis_Tool📊.py
@@ -1,40 +1,39 @@
 import streamlit as st
+from llama_parse import LlamaParse
+
+
+# def convert_pdf_to_txt(input_file, txt_output_path):
+#     documents = LlamaParse(result_type="markdown").load_data(input_file)
+#     # Merge all the text into one str
+#     all_text = []
+#     for doc in documents:
+#         all_text.append(doc.text)
+
+#     merged_doc = '\n\n'.join(all_text)
+#     # Save as txt
+#     txt_output_path = os.path.join(dir_cur, 'outputs/llama_parsed', f'{base_name}.txt')
+#     with open(txt_output_path, 'w', encoding='utf-8') as file:
+#         file.write(merged_doc)
+
+
+
 
 st.title("ESG Analysis Tool 📊")
 st.write(
     "Upload an ESG report below and see how well the company performs! "
-    "To use this app, you may need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    "To use this app, you may need to provide some API keys below. "
 )
 
 openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+llama_api_key = st.text_input("Llama Cloud API Key", type="password")
+if not (openai_api_key and llama_api_key):
+    st.info("Please add your OpenAI & Llama Cloud API key to continue.", icon="🗝️")
 else:
 
-
-    # Let the user upload a file via `st.file_uploader`.
     uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("pdf")
+        "Upload a document (PDF)", type=("pdf")
     )
 
     if uploaded_file:
         st.write("File uploaded successfully!")
 
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        # messages = [
-        #     {
-        #         "role": "user",
-        #         "content": f"Here's a document: {document} \n\n---\n\n {question}",
-        #     }
-        # ]
-
-        # Generate an answer using the OpenAI API.
-        # stream = client.chat.completions.create(
-        #     model="gpt-3.5-turbo",
-        #     messages=messages,
-        #     stream=True,
-        # )
-
-        # Stream the response to the app using `st.write_stream`.
-        # st.write_stream(stream)
